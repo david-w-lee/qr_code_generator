802dce27789f34d138481bcf82d70f1b0b7b26e1
David Lee <sea0920@hotmail.com>
2020-12-25 17:30:33 -0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using ZXing;
+
+namespace qr_code_generator
+{
+    class Program
+    {
+
+        private void QRCodeUrlTest()
+        {
+            string filename = "QRCodeUrlGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.QR_CODE, "https://www.msn.com/", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        private void QRCodeSmsTest()
+        {
+            string filename = "QRCodeSmsGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.QR_CODE, "SMSTO:+1112223333:Hello!", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        private void QRCodePhoneTest()
+        {
+            string filename = "QRCodeTelGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.QR_CODE, "tel:+1112223333", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        private void QRCodeMailTest()
+        {
+            string filename = "QRCodeMailGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.QR_CODE, "mailto:blah@hotmail.com", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        private void UPC_A_CodeTest()
+        {
+            string filename = "UpcACodeGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.UPC_A, "012345678912", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+        private void UPC_E_CodeTest()
+        {
+            string filename = "UpcECodeGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.UPC_E, "12345670", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        private void EAN_13_CodeTest()
+        {
+            string filename = "Ean13CodeGenerated.png";
+            QRCodeUtil.CreateFile(BarcodeFormat.EAN_13, "5701234567899", filename);
+            var res = QRCodeUtil.GetContent(filename);
+
+            Console.WriteLine($"Content: {res.Text}");
+            Console.WriteLine($"Format: {res.BarcodeFormat}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.QRCodeUrlTest();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QRCodeUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace qr_code_generator
+{
+    public class QRCodeUtil
+    {
+        public static Result GetContent(string filename)
+        {
+            var barcodeReader = new BarcodeReader();
+
+            var barcodeBitmap = new Bitmap(filename);
+
+            var barcodeResult = barcodeReader.Decode(barcodeBitmap);
+
+            return barcodeResult;
+        }
+
+        public static void CreateFile(BarcodeFormat format, string content, string filename)
+        {
+            var barcodeWriter = new BarcodeWriter();
+
+            barcodeWriter.Format = format;
+            barcodeWriter.Options = new QrCodeEncodingOptions
+            {
+                Margin = 1,
+                Height = 200,
+                Width = 200,
+                ErrorCorrection = ErrorCorrectionLevel.Q
+            };
+
+            var bitmap = barcodeWriter.Write(content);
+
+            bitmap.Save(filename, ImageFormat.Png);
+        }
+    }
+}
